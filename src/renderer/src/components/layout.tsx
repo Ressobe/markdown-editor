4c9afbcfe2eb6a932fa011ec26837303debb0d5c
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-03-01 12:39:12 +0100

    feat: add content and sidebar layout

--- src/renderer/src/components/layout.tsx
+++ src/renderer/src/components/layout.tsx
@@ -1,13 +1,10 @@
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function Layout({ className, children, ...props }: ComponentProps<'aside'>) {
-    return (
-        <aside 
-              className={twMerge('w-[250px] mt-10 h-[100vh + 10px] overflow-auto', className)}
-              {...props}
-        >
-            {children}
-        </aside>
-    );
+export const RootLayout = ({ children, className, ...props }: ComponentProps<'main'>) => {
+  return (
+    <main className={twMerge('flex flex-row h-screen', className)} {...props}>
+      {children}
+    </main>
+  )
 }

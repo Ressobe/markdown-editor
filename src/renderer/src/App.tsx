87f8e07efa2291e8df6cbbca64b77184411ad322
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-03-01 15:16:42 +0100

    feat: add markdown editor component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,19 @@
+import { ActionButtonRow, Content, DraggableTopBar, MarkDownEditor, NotePreviewList, RootLayout, Sidebar } from "./components"
 
 function App(): JSX.Element {
-
-  return (
-    <>
-    </>
+    return (
+      <>
+      <DraggableTopBar />
+      <RootLayout>
+        <Sidebar className="p-2 border-4 bg-zinc-800">
+          <ActionButtonRow className="flex justify-between"/>
+          <NotePreviewList className="pt-4 space-y-2" />
+        </Sidebar>
+        <Content className="bg-zinc-900/50 border-l  border-l-white/20">
+          <MarkDownEditor />
+        </Content>
+      </RootLayout>
+      </>
   )
 }
 
